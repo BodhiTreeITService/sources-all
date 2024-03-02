69ed64c7bc829e0194d8b80264e3c88af9b8db7c
BodhiTreeITService <bodhi.tree.it@foxmail.com>
2024-03-02 12:52:00 +0800

    添加根项目执行代码并调整gradle配置文件

---
+++ src/main/java/sourcesall/SourcesAllApplication.java
@@ -0,0 +1,13 @@
+package sourcesall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcesAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourcesAllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sourcesall/SourcesAllApplicationTests.java
@@ -0,0 +1,13 @@
+package sourcesall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcesAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
